94af28efcd77aa4ef689ff298bce873057f6a1eb
wlrhkd49 <wlrhkd49@naver.com>
2021-06-25 17:35:44 +0900

    adapter pattern

--- 스프링 입문/디자인 패턴/src/com/company/design/Main.java
+++ 스프링 입문/디자인 패턴/src/com/company/design/Main.java
@@ -1,5 +1,6 @@
 package com.company.design;
 
+import com.company.design.adapter.*;
 import com.company.design.singleton.AClazz;
 import com.company.design.singleton.BClazz;
 import com.company.design.singleton.SocketClient;
@@ -7,7 +8,7 @@ import com.company.design.singleton.SocketClient;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+/*	// singleton
         AClazz aClazz = new AClazz();
         BClazz bClazz = new BClazz();
 
@@ -15,6 +16,24 @@ public class Main {
         SocketClient bClient = bClazz.getSocketClient();
 
         System.out.println("두 개의 객체가 동일한가?");
-        System.out.println(aClient.equals(bClient));
+        System.out.println(aClient.equals(bClient));*/
+
+        // adapter
+
+        HairDryer hairDryer = new HairDryer();
+        connect(hairDryer);
+
+        Cleaner cleaner = new Cleaner();
+        Electronic110V adapter = new SocketAdapter(cleaner);
+        connect(adapter);
+
+        AirConditioner airConditioner = new AirConditioner();
+        Electronic110V airAdapter = new SocketAdapter(airConditioner);
+        connect(airAdapter);
+    }
+
+    // 콘센트
+    public static void connect(Electronic110V electronic110V) {
+        electronic110V.powerOn();
     }
 }
