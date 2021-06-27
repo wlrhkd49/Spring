29fab8552b9920028043fae84a2b87686a1513d7
wlrhkd49 <wlrhkd49@naver.com>
2021-06-27 15:47:02 +0900

    proxy pattern

--- 스프링 입문/디자인 패턴/src/com/company/design/Main.java
+++ 스프링 입문/디자인 패턴/src/com/company/design/Main.java
@@ -1,14 +1,20 @@
 package com.company.design;
 
 import com.company.design.adapter.*;
+import com.company.design.aop.AopBrowser;
+import com.company.design.proxy.Browser;
+import com.company.design.proxy.BrowserProxy;
+import com.company.design.proxy.IBrowser;
 import com.company.design.singleton.AClazz;
 import com.company.design.singleton.BClazz;
 import com.company.design.singleton.SocketClient;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Main {
 
     public static void main(String[] args) {
-/*	// singleton
+/*	    //싱글톤
         AClazz aClazz = new AClazz();
         BClazz bClazz = new BClazz();
 
@@ -18,9 +24,9 @@ public class Main {
         System.out.println("두 개의 객체가 동일한가?");
         System.out.println(aClient.equals(bClient));*/
 
-        // adapter
+        //어댑터
 
-        HairDryer hairDryer = new HairDryer();
+        /*HairDryer hairDryer = new HairDryer();
         connect(hairDryer);
 
         Cleaner cleaner = new Cleaner();
@@ -30,6 +36,41 @@ public class Main {
         AirConditioner airConditioner = new AirConditioner();
         Electronic110V airAdapter = new SocketAdapter(airConditioner);
         connect(airAdapter);
+
+        */
+
+        //프록시
+        /*Browser browser = new Browser("www.naver.com");
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();
+
+        IBrowser browser = new BrowserProxy("www.naver.com");
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();
+        */
+
+        AtomicLong start = new AtomicLong();
+        AtomicLong end = new AtomicLong();
+
+        IBrowser aopBrowser = new AopBrowser("www.naver.com",
+                ()-> {
+                    System.out.println("before");
+                    start.set(System.currentTimeMillis());
+                },
+                () -> {
+                    long now = System.currentTimeMillis();
+                    end.set(now - start.get());
+                });
+        aopBrowser.show();
+        System.out.println("loading time : "+end.get());
+        aopBrowser.show();
+        System.out.println("loading time : "+end.get());
+
     }
 
     // 콘센트
