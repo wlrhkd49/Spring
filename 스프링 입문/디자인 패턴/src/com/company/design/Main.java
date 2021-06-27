63990a26fc03f12ad0b23df08cb21c5e8c649b86
wlrhkd49 <wlrhkd49@naver.com>
2021-06-27 16:08:10 +0900

    decorator pattern

--- 스프링 입문/디자인 패턴/src/com/company/design/Main.java
+++ 스프링 입문/디자인 패턴/src/com/company/design/Main.java
@@ -2,6 +2,7 @@ package com.company.design;
 
 import com.company.design.adapter.*;
 import com.company.design.aop.AopBrowser;
+import com.company.design.decorator.*;
 import com.company.design.proxy.Browser;
 import com.company.design.proxy.BrowserProxy;
 import com.company.design.proxy.IBrowser;
@@ -10,6 +11,8 @@ import com.company.design.singleton.BClazz;
 import com.company.design.singleton.SocketClient;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import static java.awt.PageAttributes.MediaType.A5;
 
 public class Main {
 
@@ -52,7 +55,7 @@ public class Main {
         browser.show();
         browser.show();
         browser.show();
-        */
+
 
         AtomicLong start = new AtomicLong();
         AtomicLong end = new AtomicLong();
@@ -71,6 +74,24 @@ public class Main {
         aopBrowser.show();
         System.out.println("loading time : "+end.get());
 
+         */
+
+
+        //Decorator 패턴
+        ICar audi = new Audi(1000);
+        audi.showPrice();
+
+        // a3
+        ICar audi3 = new A3(audi, "A3" );
+        audi3.showPrice();
+
+        // a4
+        ICar audi4 = new A4(audi, "A4" );
+        audi4.showPrice();
+
+        // a5
+        ICar audi5 = new A5(audi, "A5" );
+        audi5.showPrice();
     }
 
     // 콘센트
