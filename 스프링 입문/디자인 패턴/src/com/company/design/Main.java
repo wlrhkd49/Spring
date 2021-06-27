8e08e9dd7d9a96c927318f8770678a40db466798
wlrhkd49 <wlrhkd49@naver.com>
2021-06-27 16:17:22 +0900

    observer pattern

--- 스프링 입문/디자인 패턴/src/com/company/design/Main.java
+++ 스프링 입문/디자인 패턴/src/com/company/design/Main.java
@@ -3,6 +3,8 @@ package com.company.design;
 import com.company.design.adapter.*;
 import com.company.design.aop.AopBrowser;
 import com.company.design.decorator.*;
+import com.company.design.observer.Button;
+import com.company.design.observer.IButtonListener;
 import com.company.design.proxy.Browser;
 import com.company.design.proxy.BrowserProxy;
 import com.company.design.proxy.IBrowser;
@@ -10,6 +12,7 @@ import com.company.design.singleton.AClazz;
 import com.company.design.singleton.BClazz;
 import com.company.design.singleton.SocketClient;
 
+import java.awt.*;
 import java.util.concurrent.atomic.AtomicLong;
 
 import static java.awt.PageAttributes.MediaType.A5;
@@ -74,7 +77,7 @@ public class Main {
         aopBrowser.show();
         System.out.println("loading time : "+end.get());
 
-         */
+
 
 
         //Decorator 패턴
@@ -92,6 +95,22 @@ public class Main {
         // a5
         ICar audi5 = new A5(audi, "A5" );
         audi5.showPrice();
+
+        */
+
+        //옵저버 패턴
+        Button button =new Button("버튼");
+        button.addListener(new IButtonListener() {
+            @Override
+            public void clickEvent(String event) {
+                System.out.println(event);
+            }
+        });
+        button.click("메시지 전달 : click 1");
+        button.click("메시지 전달 : click 2");
+        button.click("메시지 전달 : click 3");
+        button.click("메시지 전달 : click 4");
+
     }
 
     // 콘센트
