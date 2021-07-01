6a644e286993db0a640259acf419133a93e15275
wlrhkd49 <wlrhkd49@naver.com>
2021-07-01 17:56:36 +0900

    PutMappting

--- 스프링 입문/04. Spring Boot/실습/src/main/java/com/example/hello/controller/PutApiController.java
+++ 스프링 입문/04. Spring Boot/실습/src/main/java/com/example/hello/controller/PutApiController.java
@@ -1,4 +1,16 @@
 package com.example.hello.controller;
 
+import com.example.hello.dto.PutRequestDto;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
 public class PutApiController {
+
+    @PutMapping("/put/{user_id}")
+    public PutRequestDto put(@RequestBody PutRequestDto requestDto,
+                             @PathVariable(name = "user_id") Long id) {
+        System.out.println(id);
+        return requestDto; // 바로 JSON의 데이터로 바꿔서 response 내려감
+    }
 }
