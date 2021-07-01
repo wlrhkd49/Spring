d2a66f26cca1799b33e9061941e617c755b43d17
wlrhkd49 <wlrhkd49@naver.com>
2021-07-01 16:18:21 +0900

    Hello World API 생성

--- 스프링 입문/디자인 패턴/src/com/company/design/decorator/A5.java
+++
@@ -1,7 +0,0 @@
-package com.company.design.decorator;
-
-public class A5 extends AudiDecorator {
-    public A5(ICar audi, String modelName) {
-        super(audi, modelName, 3000);
-    }
-}
--- 스프링 입문/디자인 패턴/src/com/company/design/observer/IButtonListener.java
+++
@@ -1,5 +0,0 @@
-package com.company.design.observer;
-
-public interface IButtonListener {
-    void clickEvent(String event);
-}
