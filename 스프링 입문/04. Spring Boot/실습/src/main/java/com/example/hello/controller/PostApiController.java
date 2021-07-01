5b6450d676ef7d0f69ef54095c89b9762956f694
wlrhkd49 <wlrhkd49@naver.com>
2021-07-01 17:56:00 +0900

    PostMapping

--- 스프링 입문/04. Spring Boot/실습/src/main/java/com/example/hello/controller/PostApiController.java
+++ 스프링 입문/04. Spring Boot/실습/src/main/java/com/example/hello/controller/PostApiController.java
@@ -1,4 +1,29 @@
 package com.example.hello.controller;
 
+import com.example.hello.dto.PostRequestDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
 public class PostApiController {
+
+    @PostMapping("/post")
+    // Get일때는 RequestParam, Post는 RequestBody
+    public void post(@RequestBody Map<String, Object> requestData) {
+        requestData.forEach((key, value) -> {
+            System.out.println("key : " + key);
+            System.out.println("value : " + value);
+        });
+    }
+
+    @PostMapping("/post1")
+    // Get일때는 RequestParam, Post는 RequestBody
+    public void post1(@RequestBody PostRequestDto requestData) {
+        System.out.println(requestData);
+    }
 }
--- 스프링 입문/04. Spring Boot/실습/src/main/java/com/example/hello/dto/PostRequestDto.java
+++ 스프링 입문/04. Spring Boot/실습/src/main/java/com/example/hello/dto/PostRequestDto.java
@@ -1,4 +1,74 @@
 package com.example.hello.dto;
 
-public class PostRequestDt {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * snake 지정 방법 2가지
+ * 1) @JsonProperty("phone_number")
+ * 2) @JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
+ */
+public class PostRequestDto {
+
+    private String account;
+    private String email;
+    private String address;
+    private String password;
+
+    @JsonProperty("phone_number") // snake 케이스로 매핑
+    private String phoneNumber;
+
+    @JsonProperty("OTP")
+    private String OTP;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "PostRequestDto{" +
+                "account='" + account + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                ", password='" + password + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", OTP='" + OTP + '\'' +
+                '}';
+    }
 }
