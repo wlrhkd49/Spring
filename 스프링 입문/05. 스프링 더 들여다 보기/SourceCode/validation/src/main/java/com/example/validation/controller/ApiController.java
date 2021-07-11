54ec30529c7c6e3bf5062fc378af5b08a2e42d0d
wlrhkd49 <wlrhkd49@naver.com>
2021-07-12 00:10:40 +0900

    Spring Boot Validation

---
+++ 스프링 입문/05. 스프링 더 들여다 보기/SourceCode/validation/src/main/java/com/example/validation/controller/ApiController.java
@@ -0,0 +1,47 @@
+package com.example.validation.controller;
+
+import com.example.validation.dto.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @PostMapping("/user")
+    public ResponseEntity user(@Valid @RequestBody User user, BindingResult bindingResult) { // @Valid 어노테이션 필요
+
+        if(bindingResult.hasErrors()) {
+            StringBuilder sb = new StringBuilder();
+            bindingResult.getAllErrors().forEach(objectError -> {
+                FieldError field = (FieldError)objectError;
+                String msg = objectError.getDefaultMessage();
+
+                System.out.println("field : "+field.getField());
+                System.out.println(msg);
+
+                sb.append("field : "+field.getField());
+                sb.append(" msg : "+msg);
+            });
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(sb.toString());
+        }
+
+        System.out.println(user);
+
+        if(user.getAge() >= 90) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(user);
+        }
+        return ResponseEntity.ok(user);
+
+    }
+
+}
