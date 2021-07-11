f877a9dd6088daf208750de11b066e358f219a6b
wlrhkd49 <wlrhkd49@naver.com>
2021-07-12 00:59:10 +0900

    custom validation

--- 스프링 입문/06. 스프링 기능 활용/SourceCode/validation/src/main/java/com/example/validation/dto/User.java
+++ 스프링 입문/06. 스프링 기능 활용/SourceCode/validation/src/main/java/com/example/validation/dto/User.java
@@ -2,9 +2,9 @@ package com.example.validation.dto;
 
 import com.example.validation.annotation.YearMonth;
 
+import javax.validation.Valid;
 import javax.validation.constraints.*;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class User {
 
@@ -14,14 +14,12 @@ public class User {
     @Max(value = 90)
     private int age;
 
-    @Email
-    private String email;
+    @Valid // 오브젝트 형태라면 반드시 controller뿐만 아니라 여기도 Valid 필수!
+    private List<Car> cars;
 
-    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "핸드폰 번호의 양식과 맞지 않습니다. 01x-xxxx-xxxx")
-    private String phoneNumber;
-
-    @YearMonth
-    private String reqYearMonth; // yyyyMM
+    public List<Car> getCars() {
+        return cars;
+    }
 
     public String getName() {
         return name;
@@ -39,28 +37,8 @@ public class User {
         this.age = age;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getReqYearMonth() {
-        return reqYearMonth;
-    }
-
-    public void setReqYearMonth(String reqYearMonth) {
-        this.reqYearMonth = reqYearMonth;
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
     }
 
     @Override
@@ -68,9 +46,7 @@ public class User {
         return "User{" +
                 "name='" + name + '\'' +
                 ", age=" + age +
-                ", email='" + email + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", reqYearMonth='" + reqYearMonth + '\'' +
+                ", cars=" + cars +
                 '}';
     }
 }
