d7d4c525e6e33431cd02870500e342ca7ced60f5
wlrhkd49 <wlrhkd49@naver.com>
2021-07-16 18:43:39 +0900

    Naver 지역 검색 Api 연동

--- 스프링 입문/07. Server to Server 간의 연결/SourceCode/client/src/main/java/com/example/client/controller/ApiController.java
+++ 스프링 입문/07. Server to Server 간의 연결/SourceCode/client/src/main/java/com/example/client/controller/ApiController.java
@@ -3,7 +3,6 @@ package com.example.client.controller;
 import com.example.client.dto.Req;
 import com.example.client.dto.UserResponse;
 import com.example.client.service.RestTemplateService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- 스프링 입문/07. Server to Server 간의 연결/SourceCode/server/src/main/java/com/example/server/controller/ServerApiController.java
+++ 스프링 입문/07. Server to Server 간의 연결/SourceCode/server/src/main/java/com/example/server/controller/ServerApiController.java
@@ -3,13 +3,56 @@ package com.example.server.controller;
 import com.example.server.dto.Req;
 import com.example.server.dto.User;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpEntity;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/server")
 public class ServerApiController {
+
+    //https://openapi.naver.com/v1/search/blog.json
+    //query:\xea\xb0\x88\xeb\xb9\x84\xec\xa7\x91
+    //display:10
+    //start:1
+    //sort:sim
+    @GetMapping("/naver")
+    public String naver() {
+
+
+        String query = "갈비집";
+        String encode = Base64.getEncoder().encodeToString(query.getBytes(StandardCharsets.UTF_8));
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/search/blog.json")
+                .queryParam("query", "중국집")
+                .queryParam("display", 10)
+                .queryParam("start",1)
+                .queryParam("sort","sim")
+                .encode(Charset.forName("UTF-8"))
+                .build()
+                .toUri();
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        RequestEntity<Void> req = RequestEntity
+                .get(uri)
+                .header("X-Naver-Client-Id","_oN71avGIpftCPXw43jw")
+                .header("X-Naver-Client-Secret","dSU0HiYIxX")
+                .build();
+
+        ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+
+        return result.getBody();
+    }
 
     @GetMapping("/hello")
     public User hello(@RequestParam String name, @RequestParam int age) {
