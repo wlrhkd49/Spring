385b4151323590db6568ac9c1bc46c06a0e9cbd4
wlrhkd49 <wlrhkd49@naver.com>
2021-07-16 17:02:56 +0900

    Rest Template 사용 2

--- 스프링 입문/07. Server to Server 간의 연결/SourceCode/client/src/main/java/com/example/client/controller/ApiController.java
+++ 스프링 입문/07. Server to Server 간의 연결/SourceCode/client/src/main/java/com/example/client/controller/ApiController.java
@@ -19,6 +19,7 @@ public class ApiController {
 
     @GetMapping("/hello")
     public UserResponse getHello() {
-        return restTemplateService.hello();
+        restTemplateService.post();
+        return new UserResponse();
     }
 }
--- 스프링 입문/07. Server to Server 간의 연결/SourceCode/client/src/main/java/com/example/client/service/RestTemplateService.java
+++ 스프링 입문/07. Server to Server 간의 연결/SourceCode/client/src/main/java/com/example/client/service/RestTemplateService.java
@@ -1,5 +1,6 @@
 package com.example.client.service;
 
+import com.example.client.dto.UserRequest;
 import com.example.client.dto.UserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -36,5 +37,33 @@ public class RestTemplateService {
         System.out.println(result.getStatusCode());
         System.out.println(result.getBody());
         return result.getBody();
+    }
+
+    public void post() {
+        // http://localhost:9090/api/server/user/{userId}/name/{userName}
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/user/{userId}/name/{userName}")
+                .encode()
+                .build()
+                .expand(100,"steve") // expand는 차례대로 괄호에 매칭이 된다.
+                .toUri();
+        System.out.println(uri);
+
+        // http body -> object -> object mapper -> json -> rest template -> http body json
+        // 오브젝트로 만들어서 보내면 자연스럽게 json으로 바꿔줌
+        UserRequest req = new UserRequest();
+        req.setName("steve");
+        req.setAge(10);
+
+        RestTemplate restTemplate = new RestTemplate();
+        // 해당 uri 에 req을 보내서 해당 응답은 UserResponse로 받을 거야
+        ResponseEntity<String> response = restTemplate.postForEntity(uri, req, String.class);
+
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getHeaders());
+        System.out.println(response.getBody());
+
     }
 }
