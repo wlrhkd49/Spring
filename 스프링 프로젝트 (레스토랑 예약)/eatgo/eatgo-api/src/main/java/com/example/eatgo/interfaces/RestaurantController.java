9d863dc5be0afe43a1b56167ea9f2af625b9ebb7
wlrhkd49 <wlrhkd49@naver.com>
2021-07-27 18:18:53 +0900

    restaurant repository 생성

--- 스프링 프로젝트 (레스토랑 예약)/eatgo/eatgo-api/src/main/java/com/example/eatgo/interfaces/RestaurantController.java
+++ 스프링 프로젝트 (레스토랑 예약)/eatgo/eatgo-api/src/main/java/com/example/eatgo/interfaces/RestaurantController.java
@@ -1,6 +1,7 @@
 package com.example.eatgo.interfaces;
 
 import com.example.eatgo.domain.Restaurant;
+import com.example.eatgo.domain.RestaurantRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,23 +11,18 @@ public class RestaurantController {
 
     @GetMapping("/restaurants")
     public List<Restaurant> list() {
-        List<Restaurant> restaurants = new ArrayList<>();
-        Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
-        restaurants.add(restaurant);
+
+        RestaurantRepository repository = new RestaurantRepository();
+        List<Restaurant> restaurants = repository.findAll();
+
         return restaurants;
     }
 
     @GetMapping("/restaurants/{id}")
     public Restaurant detail(@PathVariable Long id) {
+        RestaurantRepository repository = new RestaurantRepository();
+        Restaurant restaurant = repository.findById(id);
 
-        List<Restaurant> restaurants = new ArrayList<>();
-        restaurants.add(new Restaurant(1004L, "Bob zip", "Seoul"));
-        restaurants.add(new Restaurant(2021L, "Cyber Food", "Seoul"));
-
-        Restaurant restaurant = restaurants.stream().filter(r ->
-                r.getId() == id)
-                .findFirst()
-                .orElse(null);
         return restaurant;
     }
 
